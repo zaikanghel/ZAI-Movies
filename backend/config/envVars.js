c5cdaf4c31ff5042b3da10e674b24aec738a3970
zaikanghel <105504978+zaikanghel@users.noreply.github.com>
2024-09-28 01:33:39 +0800

    Update envVars.js

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
-	PORT: process.env.PORT || 5000,
-	JWT_SECRET: process.env.JWT_SECRET,
-	NODE_ENV: process.env.NODE_ENV,
-	TMDB_API_KEY: process.env.TMDB_API_KEY,
+	MONGO_URI: "mongodb+srv://zaikanghel:mSCQZCxuqfIeRJcC@zaimovies.0xwzm.mongodb.net/netflix_db?retryWrites=true&w=majority&appName=ZaiMovies",
+	PORT: "5000" || 5000,
+	JWT_SECRET: "your_jwt_secret",
+	NODE_ENV: "production",
+	TMDB_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTNjMjIyZmZkMjg2ZjM1M2FjNTMyOTZkMjc3OWFkMyIsIm5iZiI6MTcyNzM5NzI3NS4zNzE5NjgsInN1YiI6IjY2ZjVmYmVlODU2MzNiZjkyMjk2NGEzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HYLZ3bi-o5bZK8gEddd7PU-5UUOyqIMFqV9DawpdB1M",
 };
